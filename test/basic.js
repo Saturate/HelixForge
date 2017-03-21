df9e1c4e31b1db2c84a2e933256ae3b6096d1744
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2017-03-21 17:24:32 +0100

    ES6 (#9)
    * CI configuration and readme
    
    Added CI configuration and added build status to the readme.
    
    * Fix typo in README
    
    * Add package.json
    
    Makes Appveyor build hopefully, also depend on gulp 4.
    
    * Add something to test and a basic test
    
    This should be the last thing before it all works.
    
    * ES6 for all JS
    
    Tests in ES6 and gulp in ES6

--- test/basic.js
+++ test/basic.js
@@ -1,9 +1,11 @@
 /*global describe, it*/
 'use strict';
 
+import 'babel-polyfill';
+
 describe('Smoke Test', function () {
 	it('Gulp can be required without throwing', () => {
 		// not testing the actual run of generators yet
-		require('../gulpfile');
+		require('../gulpfile.babel');
 	});
 });
