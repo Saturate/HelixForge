f0135649d6b241bd4d5475e908d30e97c779415d
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2017-03-23 21:08:27 +0100

    Add configuration builder code
    Adds basic merge code, and the start of a test. Nothing is final yet,
    only PoC.
    
    WIP on #1

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,2 +1,12 @@
 import gulp from 'gulp';
 import chalk from 'chalk';
+
+
+import ConfigurationReader from './lib/configuration-reader';
+
+const cr = new ConfigurationReader();
+
+module.exports.default = function(done) {
+	console.log('does nothing yet.');
+	done();
+}
---
+++ test/configuration-reader.js
@@ -0,0 +1,11 @@
+/*global describe, it*/
+'use strict';
+
+import 'babel-polyfill';
+import ConfigurationReader from '../lib/configuration-reader';
+
+describe('Configuration Reader', function () {
+	it('Does the stuff', () => {
+		console.log(new ConfigurationReader());
+	});
+});
