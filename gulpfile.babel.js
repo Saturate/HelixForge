d31475833463a9fbe3b84cadae1caf05380c0bc7
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2017-03-23 08:34:17 +0100

    Move code to own file
    This code does not work. Hostile does not lock the file and only adds
    one entry when you have mutriple ones. We should go back to the old code
    that I made that was a bit more messy but sync, as async does not work.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,36 +1,12 @@
 import gulp from 'gulp';
 import chalk from 'chalk';
-import hostile from 'hostile';
 
-function addHosts(done) {
+// import tasks
+import addHosts from './tasks/hostfile';
 
-	// TODO: Get data from configuration manager
-	var dummyDataHosts = [
-		{
-			ip: '127.0.0.1',
-			hostname: 'localhost.example.com'
-		}
-	];
 
-	const hosts = dummyDataHosts.slice(0); // Clone the hosts array
-	let addHostPromises = hosts.map(host => {
-		new Promise((resolve, reject) => {
-			hostile.set(host.ip, host.hostname, function (err) {
-				if (err) {
-					reject();
-					console.error(err);
-				} else {
-					console.log('Successfully added:', host.ip, host.hostname);
-					resolve();
-				}
-			});
-		});
-	});
-
-	Promise.all(addHostPromises).then(values => {
-		console.log('All hosts added.');
-		done()
-	});
+function cleanSetup () {
+	// remove hostfile entries and ISS configuration
 }
 
 exports.hosts = addHosts;
---
+++ tasks/hostfile.js
@@ -0,0 +1,42 @@
+import hostile from 'hostile';
+
+function addHosts(done) {
+
+	// TODO: Get data from configuration manager
+	var dummyDataHosts = [
+		{
+			ip: '127.0.0.1',
+			hostname: 'localhost.example.com'
+		},
+		{
+			ip: '127.0.0.1',
+			hostname: 'localhost2.example.com'
+		},
+		{
+			ip: '127.0.0.1',
+			hostname: 'localhost3.example.com'
+		}
+	];
+
+	const hosts = dummyDataHosts.slice(0); // Clone the hosts array
+	let addHostPromises = hosts.map(host => {
+		new Promise((resolve, reject) => {
+			hostile.set(host.ip, host.hostname, function (err) {
+				if (err) {
+					reject();
+					console.error(err);
+				} else {
+					console.log('Successfully added:', host.ip, host.hostname);
+					resolve();
+				}
+			});
+		});
+	});
+
+	Promise.all(addHostPromises).then(values => {
+		console.log('All hosts added.');
+		done()
+	});
+}
+
+module.exports = addHosts;
