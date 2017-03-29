1d2a3882a39032514f9042742aed6ef2effc4bfc
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2017-03-29 19:10:26 +0200

    Implement tests, and load real data
    We test the final config outputs what's expected. This will help us
    against bugs and the like!
    
    Also comment out the gulpfile, as this does not validate yet.
    
    Relates to #1

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,9 +1,13 @@
-import gulp from 'gulp';
+/*import gulp from 'gulp';
 import chalk from 'chalk';
 
 import ConfigurationReader from './lib/configuration-reader';
 
-const cr = new ConfigurationReader();
+
+const cr = new ConfigurationReader({
+
+});
+*/
 
 module.exports.default = function(done) {
 	console.log('does nothing yet.');
--- test/configuration-reader.js
+++ test/configuration-reader.js
@@ -3,9 +3,45 @@
 
 import 'babel-polyfill';
 import ConfigurationReader from '../lib/configuration-reader';
+const expect = require('chai').expect;
 
 describe('Configuration Reader', function () {
-	it('Does the stuff', () => {
-		console.log(new ConfigurationReader());
+
+	it('can create a configuration reader from the constructor', () => {
+		//new ConfigurationReader();
+	});
+
+	it('loads default configuration', () => {
+		const config = new ConfigurationReader({
+			path: './test/test-configurations/1/'
+		});
+
+		expect(config).to.have.property('ConfigKey');
+	});
+
+	it('loads default scope configuration', () => {
+		const config = new ConfigurationReader({
+			path: './test/test-configurations/1/'
+		});
+
+		expect(config).to.have.property('scopes');
+	});
+
+	it('local config overrides default configuration', () => {
+		const config = new ConfigurationReader({
+			path: './test/test-configurations/2/'
+		});
+
+		expect(config).to.have.property('ConfigKey', 'ConfigValueOverwritten');
+	});
+
+	it('local config overrides default scope configuration', () => {
+		const config = new ConfigurationReader({
+			path: './test/test-configurations/2/'
+		});
+
+		expect(config).to.have.property('scopes');
+		expect(config.scopes.Scoped).to.have.property('ScopedKey', 'ScopedValueOverwritten');
+
 	});
 });
