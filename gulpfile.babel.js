e7fcaf2ba41460f75b42570ebbc49dd697b26f1f
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2017-03-30 13:29:47 +0200

    Make host task read configuration
    This is a very basic example that just uses a default configuration
    file, it's not scoped yet. But it could be an idea, however I have no
    idea on how the scopes work best.
    
    Tried to use our logging function, it became clear that we need to read
    the log level from somewhere global. Keept the old log statements in
    comments for refence for now.
    
    Refs: #1 #2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,21 +1,20 @@
 /*import gulp from 'gulp';
 import chalk from 'chalk';
-
+*/
 import ConfigurationReader from './lib/configuration-reader';
 
 
 const cr = new ConfigurationReader({
 
 });
-*/
 
 // import tasks
 import addHosts from './tasks/hostfile';
 
-module.exports.default = function(done) {
+exports.default = function(done) {
 	console.log('does nothing yet.');
 	done();
 }
 
-exports.hosts = addHosts;
+exports.hosts = addHosts.bind(cr);
 
